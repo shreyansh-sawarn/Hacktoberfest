6094a1614da34e3e1ea80d1ba1a962b1b625b2da
sirkinshu <shreyanshsawarn88@gmail.com>
2019-04-26 23:38:51 +0530

    Avengers:Endgame

---
+++ endgame.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+printf("A program written just as a tribute to Avengers : Endgame by one their biggest fans on 26th April, 2019 \n");
+printf("Thanks to MCU for these movies and I am really going to miss the Avengers that I loved watching as I grew up. \n");
+printf("All these years, it has been a really memorable and wonderful journey and I am looking forward to great movies even after. Will definitely miss Iron Man and Captain America and Black Widow and Thor and Hulk and Hawkeye. Thanks to each of these characters because I now have some ideals in life. \n");
+printf("Finally a big thank you to Marvel and Disney. A huge respect towards Avengers : Endgame. Love from Shrey."); 
+}
