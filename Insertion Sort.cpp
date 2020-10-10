42c5dbb836add2d4e1b6f9aee5f5f80cbbc87fd6
Preetesh Verma <verma.preetesh21@gmail.com>
2020-10-10 23:33:23 +0530

    added insertion sort

---
+++ Insertion Sort.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+void insertionSort(int arr[], int n)  
+{  
+    int key;  
+    for(int i=1;i<n;i++) 
+    {  
+        key=arr[i];  
+        int j=i-1;  
+        while(j>=0 && arr[j]>key) 
+        {  
+            arr[j+1]=arr[j];  
+            j=j-1;  
+        }  
+        arr[j+1]=key;  
+    }  
+}    
+void printArray(int arr[], int n)  
+{
+    for (int i=0;i<n;i++)  
+        cout<<arr[i]<<" ";  
+    cout<<endl; 
+}  
+int main()  
+{  
+    int arr[]={12, 11, 13, 5, 6};  
+    int n=sizeof(arr)/sizeof(arr[0]);  
+    insertionSort(arr, n);  
+    printArray(arr, n);  
+    return 0;
+} 
