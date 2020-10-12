a57589d34e1a8e020d3f7d2cde1e5fa93becffd8
ppradyoth <>
2020-10-13 04:31:27 +0530

    Added bubble sort in C and CPP

---
+++ C/bubblesort.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int a[n],temp;
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n;j++){
+            if(a[j]>a[j+1]){
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    for(int i=0;i<n;i++){
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
