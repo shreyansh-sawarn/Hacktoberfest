f5358d42d82c0bf99018efd662731a611808b383
Aditya060 <51037240+Aditya060@users.noreply.github.com>
2020-10-12 12:42:31 +0530

    Create string_concatenation.java
    A very basic java to  show string concatenation.

---
+++ string_concatenation.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class String_merge
+{
+    public static void main(String s[])
+    {
+    Scanner k=new Scanner(System.in);
+    System.out.println("Enter the first String");
+    String s1=k.nextLine();
+    System.out.println("Enter the second String");
+    String s2=k.nextLine();
+    String s3=s1+s2;
+    System.out.println("The merged string is  "+ s3);
+    
+}
+}
