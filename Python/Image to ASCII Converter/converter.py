bf0ded999290cb0c15fa7a978f526a95bb059df3
Pranshu Jha <58560660+volt9801@users.noreply.github.com>
2020-10-15 09:25:48 +0530

    Create converter.py
    added a python script that converts images to ascii art

---
+++ Python/Image to ASCII Converter/converter.py
@@ -0,0 +1,22 @@
+from PIL import Image
+
+dot_char = [' ', '.']
+
+def convert(imagefile, new_width=100):
+	image = Image.open(imagefile)
+	width, height = image.size
+	ratio = width/height
+	new_height = int(new_width * ratio)
+	image = image.resize((new_width, new_height))
+
+	image = image.convert('L')
+	pixels = image.getdata()
+	characters = "".join([' ' if pixel < 100 else '.' for pixel in pixels])
+
+	pixel_count = len(characters)
+	ascii_image = "\n".join([characters[index:index+100] for index in range(0, pixel_count, 100)])
+	print(ascii_image)
+
+
+if __name__ == "__main__":
+	convert("shawn.jpg")
